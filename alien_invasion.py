0f7e88cfc87d97d762cadb680474c4100c83ef4a
Григоренко А.А <aa-grigorenko@yandex.ru>
2022-05-04 20:56:25 +0300

    3

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,13 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
 
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
+
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
@@ -15,47 +17,73 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self.screen)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запуск основного цикла игры."""
+
         while True:
             self._check_events() 
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
+
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды."""
+
+        self.bullets.update()  # Обновление позиций снарядов
+        for bullet in self.bullets.copy():   # Удаление снарядов, вышедших за край экрана.
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)   
+            print(len(self.bullets))
 
     def _check_events(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == pygame.KEYUP:
-                    self._check_keydown_events(event)    
                 elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)    
+                elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
         """Реагирует на нажатие клавиш."""
+
         if event.key == pygame.K_RIGHT: 
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
                 
     def _check_keyup_events(self, event):
         """Реагирует на отпускание клавиш."""
+
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
+
         self.screen.fill(self.settings.bg_color)  # При каждом проходе цикла перерисовывается экран.
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()  # Отображение последнего прорисованного экрана.
 
 if __name__ == '__main__':
     """"Отслеживание событий клавиатуры и мыши."""
+
     # Создание экземпляра и запуск игры.
     ai = AlienInvasion()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -6,5 +6,13 @@ class Settings():
         self.screen_width = 1200 # Параметры экрана
         self.screen_height = 800
         self.bg_color = (230, 230, 230)  # Цвет фона
+
         # Настройки корабля
         self.ship_speed = 1.5
+
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
